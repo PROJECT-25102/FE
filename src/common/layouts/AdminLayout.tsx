49303104970e8d4c7525eb210bac65103ef4f6b1
QuocDL <quoclcph18659@gmail.com>
2025-11-07 13:50:35 +0700

    TRUNG: header layout admin

--- src/common/layouts/AdminLayout.tsx
+++ src/common/layouts/AdminLayout.tsx
@@ -1,10 +1,54 @@
-import { ConfigProvider, Layout, theme } from "antd";
-import { Outlet } from "react-router";
+import {
+  HomeOutlined,
+  LeftOutlined,
+  LogoutOutlined,
+  ReloadOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import {
+  App,
+  Avatar,
+  Button,
+  ConfigProvider,
+  Dropdown,
+  Layout,
+  theme,
+  type MenuProps,
+} from "antd";
+import { Link, Outlet, useNavigate } from "react-router";
+import { useAuthSelector } from "../stores/useAuthStore";
 import SideBar from "./components/sideBar/SideBar";
 
 const { Content } = Layout;
 
 const AdminLayout = () => {
+  const { user, logout } = useAuthSelector((state) => ({
+    user: state.user,
+    logout: state.logout,
+  }));
+  const nav = useNavigate();
+  const items: MenuProps["items"] = [
+    {
+      label: <Link to={"/profile"}>Thông tin</Link>,
+      icon: <UserOutlined />,
+      key: "1",
+    },
+    {
+      type: "divider",
+    },
+    {
+      label: "Đăng xuất",
+      key: "3",
+      icon: <LogoutOutlined className="rotate-180" />,
+      onClick: () => {
+        logout();
+        nav("/");
+      },
+    },
+  ];
+  const handleReload = () => {
+    window.location.reload();
+  };
   return (
     <div className="bg-[#10141b] min-h-screen text-white">
       <ConfigProvider
@@ -23,19 +67,46 @@ const AdminLayout = () => {
           },
         }}
       >
-        <Layout style={{ minHeight: "100dvh" }}>
-          <SideBar />
-          <Layout style={{ marginLeft: 280 }}>
-            <Content
-              style={{
-                padding: 24,
-                background: "#10141b",
-              }}
-            >
-              <Outlet />
-            </Content>
+        <App>
+          <Layout style={{ minHeight: "100dvh" }}>
+            <SideBar />
+            <Layout style={{ marginLeft: 280 }}>
+              <Content
+                style={{
+                  padding: 24,
+                  background: "#10141b",
+                }}
+              >
+                <div className="bg-[#121822] w-full  rounded-md shadow-md px-6 py-3 mb-4 flex items-center justify-between">
+                  <div className="flex items-center gap-2">
+                    <Link to="/admin">
+                      <Button icon={<HomeOutlined />} />
+                    </Link>
+                    <Button onClick={() => nav(-1)} icon={<LeftOutlined />} />
+                    <Button onClick={handleReload} icon={<ReloadOutlined />} />
+                  </div>
+                  <div>
+                    <Dropdown menu={{ items }}>
+                      <Link
+                        to={"/profile"}
+                        className=" flex items-center gap-3 font-medium cursor-pointer hover:opacity-85 duration-300  px-6  rounded-full text-white!"
+                      >
+                        <Avatar src={user?.avatar} size="default" />
+                        <div className="flex flex-col">
+                          <p>Xin chào</p>
+                          <p className="whitespace-nowrap text-ellipsis max-w-[120px] overflow-hidden">
+                            {user?.userName}
+                          </p>
+                        </div>
+                      </Link>
+                    </Dropdown>
+                  </div>
+                </div>
+                <Outlet />
+              </Content>
+            </Layout>
           </Layout>
-        </Layout>
+        </App>
       </ConfigProvider>
     </div>
   );
--- src/common/layouts/components/sideBar/SideBar.tsx
+++ src/common/layouts/components/sideBar/SideBar.tsx
@@ -1,28 +1,22 @@
-import { Button, Layout } from "antd";
-import { Link, useNavigate } from "react-router";
-import { useAuthSelector } from "../../../stores/useAuthStore";
+import { Layout } from "antd";
+import { Link } from "react-router";
 import { menuGroups } from "./_options";
 import SideBarItems from "./SideBarItems";
 const { Sider } = Layout;
 
 const SideBar = () => {
-  const { logout } = useAuthSelector((state) => ({
-    logout: state.logout,
-  }));
-  const nav = useNavigate();
   return (
     <Sider
       width={280}
       style={{
         position: "fixed",
         height: "100dvh",
-        overflowY: "auto",
-        background: "#121822",
+        padding: "24px 0px 24px 16px",
+        background: `#10141b`,
       }}
-      className="shadow-md"
     >
-      <div>
-        <div className="px-6 pt-4 pb-4 border-b border-gray-200">
+      <div className="shadow-md  w-full h-full bg-[#121822] rounded-md">
+        <div className="px-6 pt-4 pb-4 border-b border-gray-200/30">
           <div className="text-lg font-medium flex items-center gap-3">
             <Link
               to={"/"}
@@ -43,18 +37,6 @@ const SideBar = () => {
             ))}
           </ul>
         </div>
-      </div>
-      <div className="px-6 absolute bottom-8 w-full">
-        <Button
-          onClick={() => {
-            nav("/");
-            logout();
-          }}
-          className="w-full! text-primary!"
-          type="dashed"
-        >
-          Đăng xuất
-        </Button>
       </div>
     </Sider>
   );
