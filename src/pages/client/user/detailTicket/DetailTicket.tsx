f43872176a05b8929ab014d3509121f6cfbcd767
QuocDL <quoclcph18659@gmail.com>
2025-12-10 12:22:07 +0700

    FIX DETAIL TICKET: QR CODE VALUE, STATUS TICKET

--- src/pages/client/user/detailTicket/DetailTicket.tsx
+++ src/pages/client/user/detailTicket/DetailTicket.tsx
@@ -1,16 +1,17 @@
-import { CheckCircleOutlined } from "@ant-design/icons";
+import { CheckCircleOutlined, CloseCircleOutlined } from "@ant-design/icons";
 import { useQuery } from "@tanstack/react-query";
 import { Button, QRCode, Table } from "antd";
 import dayjs from "dayjs";
 import { Link, useParams } from "react-router";
 import { DAYOFWEEK_LABEL } from "../../../../common/constants/dayOfWeek";
 import { QUERYKEY } from "../../../../common/constants/queryKey";
+import { STATUS_TICKET } from "../../../../common/constants/ticket";
 import { getDetailMyTicket } from "../../../../common/services/user.service";
+import { formatCurrency } from "../../../../common/utils";
 import {
   customStatusRender,
   getStatusQrCode,
 } from "../../../../common/utils/qrCode";
-import { formatCurrency } from "../../../../common/utils";
 const column = [
   {
     title: "Phòng chiếu",
@@ -45,22 +46,46 @@ const DetailTicket = () => {
     <div className="max-w-7xl xl:mx-auto mx-6">
       <div className="flex items-center mt-20">
         <div className="flex flex-col flex-1/4 gap-2 items-center justify-center">
-          <CheckCircleOutlined className="text-green-500! text-7xl" />
-          <p className="text-2xl text-white font-bold">Giao dịch thành công</p>
-          <p className="text-center text-base">
-            Cảm ơn quý khách đã đặt vé
-            <br />
-            Quý khách vui lòng lưu lại thông tin mã vé trên màn hình để lấy vé
-            vào xem
-            <br />
-            Moi vấn đề cần giải đáp, xin vui lòng liên hệ: 024.3514791
-          </p>
+          {data && data.data.status === STATUS_TICKET.CANCELLED ? (
+            <>
+              <CloseCircleOutlined className="text-red-500! text-7xl" />
+              <p className="text-2xl text-white font-bold">Vé đã bị huỷ</p>
+
+              <p className="text-center text-base text-red-400">
+                Lý do huỷ:
+                <br />
+                <span className="font-medium text-white">
+                  {data.data.cancelDescription || "Không có lý do"}
+                </span>
+              </p>
+              <p className="text-center text-base">
+                Mọi vấn đề cần giải đáp, xin vui lòng liên hệ: 024.3514791
+              </p>
+            </>
+          ) : (
+            <>
+              <CheckCircleOutlined className="text-green-500! text-7xl" />
+              <p className="text-2xl text-white font-bold">
+                Giao dịch thành công
+              </p>
+              <p className="text-center text-base">
+                Cảm ơn quý khách đã đặt vé
+                <br />
+                Quý khách vui lòng lưu lại thông tin mã vé trên màn hình để lấy
+                vé vào xem
+                <br />
+                Mọi vấn đề cần giải đáp, xin vui lòng liên hệ: 024.3514791
+              </p>
+            </>
+          )}
+
           <Link to={"/contact"}>
             <Button type="primary" className="px-8! rounded-full! mt-2">
               Liên hệ
             </Button>
           </Link>
         </div>
+
         <div className="bg-[#1a1d23] flex-1 p-6 rounded-xl">
           <h2 className="text-center text-lg font-semibold">Thông tin vé</h2>
           <p className="mt-4 uppercase font-bold text-lg line-clamp-1">
@@ -74,11 +99,11 @@ const DetailTicket = () => {
               </p>
               <p className="text-gray-300/50 mt-4 text-base">Mã đặt vé</p>
               <p className="font-semibold text-base uppercase">
-                {data?.data._id}
+                {data?.data.ticketId || data?.data._id}
               </p>
             </div>
             <QRCode
-              value="http://localhost:5173"
+              value={data?.data.qrCode as string}
               size={120}
               status={getStatusQrCode(data?.data.status)}
               statusRender={customStatusRender}
@@ -92,9 +117,16 @@ const DetailTicket = () => {
             {DAYOFWEEK_LABEL[dayjs(data?.data.startTime).day()]} -{" "}
             {dayjs(data?.data.startTime).format("DD/MM/YYYY")}
           </p>
-          <p className=" mt-4 text-lg text-blue-400">
-            Khán giả vui lòng dùng mã này lên thẳng phòng chiếu. Không cần đổi
-            sang vé giấy
+          {data?.data.cancelDescription && (
+            <p className="text-gray-300/50 text-base mt-4">Lý do huỷ</p>
+          )}
+          <p
+            className={` text-lg ${data?.data.cancelDescription ? "text-red-500" : "text-blue-500 mt-4"}`}
+          >
+            {data?.data.cancelDescription
+              ? data.data.cancelDescription
+              : ` Khán giả vui lòng dùng mã này lên thẳng phòng chiếu. Không cần đổi
+            sang vé giấy`}
           </p>
           <div className="mt-4">
             <Table
